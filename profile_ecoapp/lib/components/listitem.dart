965f672807635184fe25eb5e1c8c92bb9c5be061
sebasPolar <sebasgomez1099@gmail.com>
2022-02-21 15:41:24 -0500

    rutas, perfil, eventos listos, se esta por terminar news

---
+++ profile_ecoapp/lib/components/listitem.dart
@@ -0,0 +1,8 @@
+class ListItem {
+  late String imgUrl;
+  late String newsTitle;
+  late String author;
+  late String date;
+
+  ListItem(this.imgUrl, this.newsTitle, this.author, this.date);
+}
--- profile_ecoapp/lib/src/pages/news.dart
+++ profile_ecoapp/lib/src/pages/news.dart
@@ -1,6 +1,6 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'package:profile_ecoapp/components/listitem.dart';
+import 'package:profile_ecoapp/components/listwidget.dart';
 
 class News extends StatefulWidget {
   News({Key? key}) : super(key: key);
@@ -10,6 +10,39 @@ class News extends StatefulWidget {
 }
 
 class _NewsState extends State<News> with SingleTickerProviderStateMixin {
+  List<ListItem> listTiles = [
+    ListItem(
+      "https://images.unsplash.com/photo-1571942347102-23df2d0ee3a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      "Noticia 1",
+      "Fernado",
+      "28 Jan 2020",
+    ),
+    ListItem(
+      "https://images.unsplash.com/photo-1571942347102-23df2d0ee3a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      "Noticia 1",
+      "Fernado",
+      "28 Jan 2020",
+    ),
+    ListItem(
+      "https://images.unsplash.com/photo-1571942347102-23df2d0ee3a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      "Noticia 1",
+      "Fernado",
+      "28 Jan 2020",
+    ),
+    ListItem(
+      "https://images.unsplash.com/photo-1571942347102-23df2d0ee3a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      "Noticia 1",
+      "Fernado",
+      "28 Jan 2020",
+    ),
+    ListItem(
+      "https://images.unsplash.com/photo-1571942347102-23df2d0ee3a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      "Noticia 1",
+      "Fernado",
+      "28 Jan 2020",
+    ),
+  ];
+
   List<Tab> _tabList = [
     Tab(
       child: Text('Medio ambiente'),
@@ -42,6 +75,7 @@ class _NewsState extends State<News> with SingleTickerProviderStateMixin {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        toolbarHeight: 80.0,
         centerTitle: true,
         title: Text(
           'News',
@@ -54,10 +88,37 @@ class _NewsState extends State<News> with SingleTickerProviderStateMixin {
           child: TabBar(
             indicatorColor: Colors.black,
             isScrollable: true,
+            labelColor: Colors.black,
             controller: _tabController,
             tabs: _tabList,
           ),
         ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Container(
+              child: ListView.builder(
+                  itemCount: _tabList.length,
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () {},
+                      child: listWidget(listTiles[index]),
+                    );
+                  }),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Container(),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Container(),
+          ),
+        ],
       ),
     );
   }
