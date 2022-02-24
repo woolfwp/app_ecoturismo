cf0cd588ffd80e4031de8a771111e7cafba447e7
sebasPolar <sebasgomez1099@gmail.com>
2022-02-24 15:20:07 -0500

    rutas, perfil, eventos, news queda pendiente inicio de notificaciones, se añade funcion de registro y login

--- profile_ecoapp/lib/main.dart
+++ profile_ecoapp/lib/main.dart
@@ -1,20 +1,35 @@
+// @dart=2.9
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:profile_ecoapp/src/pages/home.dart';
+import 'package:profile_ecoapp/src/pages/login.dart';
+import 'package:profile_ecoapp/src/pages/news.dart';
 import 'package:profile_ecoapp/src/pages/profile.dart';
 import 'package:profile_ecoapp/src/pages/events.dart';
+import 'package:profile_ecoapp/src/pages/signup.dart';
 
-void main() => runApp(MyApp());
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: 'home',
+      initialRoute: 'login',
       routes: <String, WidgetBuilder>{
+        'login': (BuildContext context) => LoginPage(),
+        'signup': (BuildContext context) => SignupPage(),
         'home': (BuildContext context) => HomePage(),
         'profile': (BuildContext context) => Profile(),
         'events': (BuildContext context) => Events(),
+        'news': (BuildContext context) => News(),
       },
     );
   }
